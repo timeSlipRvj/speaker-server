886b2fea82a1441000e6dbafc2b46eee677b507e
timeSlipRvj <103722237+timeSlipRvj@users.noreply.github.com>
2022-05-30 23:56:35 +0530

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -12,7 +12,7 @@ export const typeOrmConfig = (): TypeOrmModuleOptions => {
     return { 
         type: 'postgres',
         host: process.env.PG_HOST,
-        port: 5432,
+        port: 25060,
         username: process.env.PG_USERNAME,
         password: process.env.PG_PASSWORD,
         database: process.env.PG_DATABASE,
